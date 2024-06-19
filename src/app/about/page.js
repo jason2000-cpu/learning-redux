7cf20037a9f122ef1660131a4b20ffaa6962ba6f
jason2000-cpu <jasonmwangi2000@gmail.com>
2024-06-19 16:42:33 +0300

    added about page

---
+++ src/app/about/page.js
@@ -0,0 +1,35 @@
+'use client'
+
+import React, { useState } from "react"
+import { useDispatch } from "react-redux"
+import { useRouter } from "next/navigation"
+import { useAppSelector } from "@/lib/hooks"
+import { selectUser } from "@/lib/features/user/userSlice"
+
+
+function About(){
+    const user = useAppSelector(selectUser);
+    const dispatch = useDispatch();
+    const router = useRouter();
+
+    const handleLogout = () =>{
+        dispatch(logout)
+        alert(`See you next time ${user.username}`)
+        router.push("/auth/login")
+    }
+    return (
+        <div className="flex flex-col justify-center items-center space-y-8">
+            <span className="font-bold  text-xl">Dashboard Page</span>
+            <div className="space-y-4 text-center">
+                <h1>Hello {user.username}</h1>
+                <p>Your password is {user.password}</p>
+            </div>
+            <button 
+                type="submit"
+                onClick={handleLogout}
+            >Logout</button>
+        </div>
+    )
+}
+
+export default About;
--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -5,6 +5,19 @@ import { useDispatch } from "react-redux";
 import { useRouter } from "next/navigation";
 import { useAppSelector } from "@/lib/hooks";
 import { logout, selectUser } from "@/lib/features/user/userSlice";
+import Link from "next/link";
+
+const navs = [
+    {
+        name: "Home",
+        path: "/dashboard"
+    },
+    {
+        name: "About",
+        path: "/about"
+    }
+]
+
 function Dashboard(){
     const user = useAppSelector(selectUser)
     const dispatch = useDispatch();
@@ -17,6 +30,15 @@ function Dashboard(){
     }
     return (
         <div className="flex flex-col justify-center items-center space-y-8">
+            <div className="flex space-x-3">
+                {
+                    navs.map((nav, key) => {
+                        return (
+                            <Link href={nav.path}>{nav.name}</Link>
+                        )
+                    })
+                }
+            </div>
             <span className="font-bold  text-xl">Dashboard Page</span>
             <div className="space-y-4 text-center">
                 <h1>Hello {user.username}</h1>
